f369e9359b7a0cecba55869dbd0be98b91aa6c00
pangym <pangym@tfinfo.cn>
2019-08-20 18:06:44 +0800

    首页提交

--- src/views/index/home/home.vue
+++ src/views/index/home/home.vue
@@ -7,7 +7,29 @@
  * @LastEditTime: 2019-08-18 17:48:02
  -->
 <template>
-  <div>只是主页</div>
+  <div id="mainHome">
+    <div class='logoNav clearfix'>
+      <p class='logoTit'>
+        <span>易推广</span>
+      </p>
+      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+        <el-menu-item index="1">产品特性</el-menu-item>
+        <el-menu-item index="2">我的站点</el-menu-item>
+        <el-menu-item index="3">数据统计</el-menu-item>
+      </el-menu>
+    </div>
+    <div class='startCon clearfix'>
+      <div class='leftDiv'> 
+        <span class='line1'></span>
+        <p>十分钟完成<br/>智能营销页搭建</p>
+        <span class='line2'></span>
+        <button class='startBtn' @click="startOnce()">立即开始</button>
+      </div>
+      <div class='rightDiv'>
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -15,7 +37,7 @@ export default {
   name: 'Main',
   data () {
     return {
-
+      activeIndex:"1"
     }
   },
   components: {
@@ -27,10 +49,21 @@ export default {
 
   },
   methods: {
-
+    //立即开始
+    startOnce(){
+      this.$router.push({
+       path: '/control'
+      })
+    },
+    handleSelect(key, keyPath){
+      if(key == 2){
+        this.$router.push({path: '/control'})
+      }
+    }
   }
 }
 </script>
 
-<style rel="stylesheet/scss" lang="scss">
+<style rel="stylesheet/scss" lang="scss" scoped>
+@import "./home.scss";
 </style>
