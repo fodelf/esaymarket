2476865dc79898fea629ec5e4e18183eeb269dcd
fodelf <310066827@qq.com>
2019-08-28 10:14:11 +0800

    登陆修改

--- src/views/index/login/login.js
+++ src/views/index/login/login.js
@@ -4,7 +4,7 @@
  * @Github: https://github.com/fodelf
  * @Date: 2019-08-27 19:10:55
  * @LastEditors: 吴文周
- * @LastEditTime: 2019-08-27 20:27:41
+ * @LastEditTime: 2019-08-28 10:11:05
  */
 import { login } from '@/api/index/login.js'
 import { setToken } from '@/utils/auth'
@@ -35,7 +35,7 @@ export default {
       this.$refs.loginForm.validate(valid => {
         if (valid) {
           var param = {
-            userName: this.ruleForm.userName,
+            username: this.ruleForm.userName,
             password: md5(this.ruleForm.password)
           }
           login(param)
